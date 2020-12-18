e68ed4ddf42dfc7b1dbbf8566d001c7d759e17a1
Zach <51764547+zhenry9@users.noreply.github.com>
2020-12-18 13:45:59 -0800

    Add logo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -57,6 +57,8 @@ exclude_patterns = []
 #
 html_theme = 'sphinx_rtd_theme'
 
+html_logo = mammoth-logo.jpg
+
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
